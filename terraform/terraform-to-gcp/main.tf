12b7abf9f5ff8899641839fa10926915ac1f6f3d
Yurii Kovtsun <bobrja@gmail.com>
2023-09-06 01:32:09 +0300

    add subnetwork

--- terraform/terraform-to-gcp/main.tf
+++ terraform/terraform-to-gcp/main.tf
@@ -17,6 +17,13 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+    name = var.subnet_name
+    ip_cidr_range = var.subnet_cidr
+    network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_instance" "vm_instance" {

56c7877e3fb77207d8b71452f98ffb08b4a44c94
Yurii Kovtsun <bobrja@gmail.com>
2023-09-10 21:04:46 +0300

    upd

--- docker/docker-compose/docker-app-with-compose/docker-compose-app/app/main.py
+++ docker/docker-compose/docker-app-with-compose/docker-compose-app/app/main.py
@@ -1,6 +1,13 @@
 from pymongo import MongoClient
 
+sensor_1 = {"id": "001", "name": "Temp", "location": "room1", "Value": "18"}
+sensor_2 = {"id": "002", "name": "Temp", "location": "room2", "Value": "22"}
+
 db_client = MongoClient('localhost', 27017)
-db = client.admin
-dbs_list = db.command('listDatabases')
-pprint(dbs_list)
+
+data = db_client.detals
+ 
+storage = data.storage
+
+status = storage.insert_one(sensor_1)
+print(status)

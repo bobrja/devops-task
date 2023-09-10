d691abcd90bb60f95b7bd640b74f365f33058173
Yurii Kovtsun <bobrja@gmail.com>
2023-09-10 20:55:29 +0300

    edit main.py

--- docker/docker-compose/docker-app-with-compose/docker-compose-app/app/main.py
+++ docker/docker-compose/docker-app-with-compose/docker-compose-app/app/main.py
@@ -1,8 +1,6 @@
 from pymongo import MongoClient
-from pprint import pprint
 
-MONGO_URL = "mongodb://mongo:27017"
-client = MongoClient(MONGO_URL)
+db_client = MongoClient('localhost', 27017)
 db = client.admin
 dbs_list = db.command('listDatabases')
 pprint(dbs_list)
